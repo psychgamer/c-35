1e5defedc45ad64ee61e88ef28e8281c5b9c9657
psychgamer <75430505+psychgamer@users.noreply.github.com>
2021-04-17 14:58:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var ball,database,ballposition,position;
+
+function setup(){
+    database = firebase.database();
+    
+    createCanvas(500,500);
+    
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    ballposition = database.ref('ball/position');
+    ballposition.on('value',readposition,showerror);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+}
+function readposition(){
+
+    
+}
